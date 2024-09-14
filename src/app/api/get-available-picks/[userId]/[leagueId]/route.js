ae7402b15cb3025f73020f2f0c5cedf2d8e1dd6e
Elliot Jasper <elliotjasperis@gmail.com>
2024-09-14 01:03:17 +0100

    fixes to getting weekly games. DB query fixed

--- src/app/api/get-available-picks/[userId]/[leagueId]/route.js
+++ src/app/api/get-available-picks/[userId]/[leagueId]/route.js
@@ -28,10 +28,8 @@ export async function GET(req, { params }) {
 
   // Extract team names
   const teamNames = picks.map((pick) => pick.teamName);
-
   // Filter out teams that have already been picked
   const filteredAvailablePicks = availablePicks.filter((pickableTeam) => !teamNames.includes(pickableTeam));
-
   // Return the filtered available picks
   return new Response(JSON.stringify({ userId, leagueId, availablePicks: filteredAvailablePicks }), {
     status: 200,
