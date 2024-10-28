15b453cf4272de5321b3114fdf673a21c23ad201
Elliot <elliotjasperis@gmail.com>
2024-10-28 20:42:40 +0000

    Created a dateHandler class to get and format dates

---
+++ lib/dateHandler.ts
@@ -0,0 +1,39 @@
+export class DateHandler {
+  /**
+   *
+   * @returns {string[]} - An array of dates from today until the next Sunday
+   */
+  static generateDatesUntilSunday(): string[] {
+    const formattedDates: string[] = [];
+    const dateObj = new Date();
+    const currentDay = dateObj.getDay();
+    const daysUntilSunday = 8 - currentDay;
+
+    for (let i = 0; i < daysUntilSunday; i++) {
+      const newDate = new Date(dateObj);
+      newDate.setDate(dateObj.getDate() + i);
+      formattedDates.push(newDate.toISOString());
+    }
+
+    return formattedDates;
+  }
+
+  /**
+   *
+   * @returns {string[]} - An array of dates from today until the previous Sunday
+   */
+  static generateDatesUntilPreviousSunday(): string[] {
+    const formattedDates: string[] = [];
+    const dateObj = new Date();
+    const currentDay = dateObj.getDay();
+    const daysSinceSunday = currentDay === 0 ? 7 : currentDay;
+
+    for (let i = 0; i < daysSinceSunday; i++) {
+      const newDate = new Date(dateObj);
+      newDate.setDate(dateObj.getDate() - i);
+      formattedDates.push(newDate.toISOString());
+    }
+
+    return formattedDates;
+  }
+}
